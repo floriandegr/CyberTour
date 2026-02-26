308666140f8ba57f9f3631e21e8b0cb3b88bfe93
Falco-12 <falco.soetaert@student.vives.be>
2026-02-26 11:00:31 +0100

    form

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+const setup = () => {
+    let knop1 =document.getElementById("btn1")
+
+
+    knop1.addEventListener("click", testfunctie)
+
+
+}
+let isingedrukt = true
+const testfunctie = () =>{
+isingedrukt = !isingedrukt;
+console.log(isingedrukt);
+}
+
+
+window.addEventListener("load", setup);
